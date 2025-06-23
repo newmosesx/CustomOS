a417a084fdcdf140d5357ee4620aa0f103e9933f
Tracsbrum <newlifemoses4@gmail.com>
2025-06-23 00:27:17 +0000

    Small rearrenging.

---
+++ customOS/kernel/GDT_main.c
@@ -0,0 +1,5 @@
+// Here we will start the first process for booting
+// C recap: https://youtu.be/dTp0c41XnrQ?si=r2FUvGQrQBXXqdnX
+// Short Explanation: https://youtu.be/MKV_patjMcM?si=BJGybr5JAVASbr9m
+// Long Implementation: https://youtu.be/jwulDRMQ53I?si=BUvXtKm0QDgXIM-t
+// Dive in bits protected mode: https://youtu.be/Wh5nPn2U_1w?si=pVMKDBKcc5MBWlCy
